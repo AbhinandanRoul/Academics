88b5f4d315e44742096f8641de2dd1660eced545
AbhinandanRoul <53302086+AbhinandanRoul@users.noreply.github.com>
2020-05-11 20:36:58 +0530

    Create ITER_Minor_Project_May_2020.java

---
+++ ITER_Minor_Project_May_2020.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+
+class Node {
+	
+	int coef;
+	int exp;
+	Node next;
+}
+
+public class polynomial
+{
+	static Node poly1 = null;
+	static Node poly2= null;
+	static Node q;
+	static Scanner sc = new Scanner(System.in);
+	public static Node create(Node start) 
+	{
+		
+		int ch;
+		Node p = new Node();
+		//System.out.println("Accepting Data for Polynomial");
+		System.out.println("Enter the 1st term of the polynomial");
+		System.out.println("Input coef");
+		p.coef = sc.nextInt();
+		System.out.println("Input Exponent");
+	    p.exp=sc.nextInt();
+		p.next = null;
+		if (start == null) {
+			start = p;		
+			q = p;
+		}
+	
+		System.out.println("To continue press 1");
+		System.out.println("To start entering data for new polynomial press 2");
+		ch=sc.nextInt();
+		while(ch==1)
+		{
+			p=new Node();
+			System.out.println("Enter new term of same polynomial");
+			System.out.println("Enter coefficient");
+			p.coef=sc.nextInt();
+			System.out.println("Enter Exponent");
+			p.exp=sc.nextInt();
+			p.next=null;
+			q.next=p;
+			q=p;
+			System.out.println("To continue press1");
+			System.out.println("To start entering data for new polynomial press 2");
+			ch=sc.nextInt();
+		}
+		return start;
+	}
+	
+
+	
+	public static void display(Node start) 
+	{
+		System.out.println("The polynomial is:");
+		Node s = start;
+		while (s != null) {
+			System.out.print(s.coef+"X^"+s.exp+"+");
+			s = s.next;
+		}
+		System.out.println("0");
+	}
+	
+	
+	
+	
+	static void poly_add(Node poly1, Node poly2)
+	{
+		Node ptr1;
+		Node ptr2;
+		ptr1=poly1;
+		ptr2=poly2;
+		Node ptr=new Node();
+		Node poly=ptr;
+		
+		while((ptr1.next!=null) || (ptr2.next!=null))
+		{
+			if(ptr1.exp>ptr2.exp) // The term with higher exponent is added first
+			{
+				ptr.coef=ptr1.coef;
+				ptr.exp=ptr1.exp;
+				ptr1=ptr1.next;
+			}
+			else if(ptr1.exp<ptr2.exp)// The term with higher exponent is added first
+			{
+				ptr.coef=ptr2.coef;
+				ptr.exp=ptr2.exp;
+				ptr2=ptr2.next;
+			}
+			else
+			{
+				// In case if both the terms have same exponent then the coefficients are added
+				ptr.coef=ptr1.coef+ptr2.coef;
+				ptr.exp=ptr1.exp;
+				ptr1=ptr1.next;
+				ptr2=ptr2.next;
+				
+			}
+			
+			ptr.next=new Node();
+			ptr=ptr.next;
+			
+			
+		}
+		
+		if(ptr1.next==null || ptr2.next==null)
+		{
+			if(ptr1.exp==ptr2.exp)
+			{
+				ptr.next=new Node();
+				ptr=ptr.next;
+				ptr.coef=ptr1.coef+ptr2.coef;
+			
+			}
+			else
+			{
+				ptr.next=new Node();
+				ptr=ptr.next;
+				ptr.coef=ptr1.coef;
+				ptr.exp=ptr1.exp;
+				ptr.next=new Node();
+				ptr=ptr.next;
+				ptr.coef=ptr2.coef;
+				ptr.exp=ptr2.exp;
+			}
+		}	
+		ptr.next=null;
+			
+	display(poly);
+
+	}
+
+
+public static void main(String[] args) {
+	System.out.println("Accepting data for 1st Polynomial");
+		poly1=create(poly1);
+		display(poly1);
+		System.out.println("Accepting Data for 2nd Polynomial");
+		poly2=create(poly2);
+		display(poly2);
+		System.out.println("After ADDITION");
+		poly_add(poly1, poly2);
+		}
+ }
